3cb2cf75b0fd31b36859caf86b5648fc5290fc52
Dmytro-1506 <kyunlik@gmail.com>
2024-12-10 21:49:20 +0100

    event listeners logik

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,55 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    let modalIsOpen = false;
+    const refs = {
+        openModalBtn1: document.querySelector("[data-modal-open1]"),
+        openModalBtn2: document.querySelector("[data-modal-open2]"),
+        openModalBtn3: document.querySelector("[data-modal-open3]"),
+        openModalBtn4: document.querySelector("[data-modal-open4]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn1.addEventListener("click", openModal);
+    refs.openModalBtn2.addEventListener("click", openModal);
+    refs.openModalBtn3.addEventListener("click", openModal);
+    refs.openModalBtn4.addEventListener("click", openModal);
+    refs.closeModalBtn.addEventListener("click", closeModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+    function handleEsc(e) {
+        if (e.key === "Escape") {
+            closeModal();
+        }
+    }
+
+    function openModal() {
+        if (!modalIsOpen) {
+            modalIsOpen = true
+            toggleModal();
+            const backdrop = document.querySelector(".backdrop");
+
+            backdrop.addEventListener("click", handleBackdrop);
+
+            function handleBackdrop(event) {
+                console.log(event.target);
+                console.log(backdrop);
+                if (event.target === backdrop) {
+                    closeModal();
+                }
+            }
+
+            document.addEventListener("keydown", handleEsc)
+        }
+    }
+
+    function closeModal() {
+        if (modalIsOpen) {
+            modalIsOpen = false;
+            toggleModal();
+            document.removeEventListener("keydown", handleEsc);
+        }
+    }
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
 })();
